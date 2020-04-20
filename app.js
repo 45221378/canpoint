ed3a82f72ce2e5b72addf0f8330a6c9ea9d04674
meiyulu <45221378@qq.com>
2020-04-20 12:12:07 +0800

    0420

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ App({
     // wx.setStorageSync('requstURL', 'http://zywx.canpoint.net:9200/')
 
     wx.setStorageSync('requstURL', 'https://zyzs.canpoint.net/')
+    
   },
+
+  
   globalData: {
     userInfo: null
   }
